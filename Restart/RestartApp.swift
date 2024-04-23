5f97bff770f44bbc244693307c3b22bc35545ed0
Joel Lacerda <joellacerdaol@gmail.com>
2024-04-23 11:52:09 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Joel Lacerda on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
