4aed30a4ff7d80b518322acea77b739828e5e244
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-05 19:43:10 -0300

    feat: home view

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -12,13 +12,38 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            Spacer()
+            ZStack {
+                CircleGroupView(shapeColor: .gray, shapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            // MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            Spacer()
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
     }
 }
---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,32 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Joel Lacerda on 05/05/24.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    @State var shapeColor: Color
+    @State var shapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color("ColorBlue")
+            .ignoresSafeArea(.all, edges: .all)
+        CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+    }
+}
